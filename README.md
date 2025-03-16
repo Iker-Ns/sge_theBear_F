8667a10f8eae764b8bb9a0276cff4f7df9d963f2
ernestodiaz22 <ernestodiaz22@ilg.cat>
2025-03-16 22:09:00 +0100

    ACTIVITAT - FASTAPI + BD parte de Ernesto Díaz Jiménez

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # ACTIVITAT-5-PYTHON-POSTGRESQL
 <img src="image.png"></img>
 
+# ACTIVITAT - FASTAPI + BD
+
+<h4>Inicio de la app</h4>
+<img src="image1.png"></img>
+<h4>Ejecución del endpoint de post add</h4>
+<img src="image2.png"></img>
+<h4>Respuesta del endpoint en consola</h4>
+<img src="image3.png"></img>
+<h4>Ejecución del endpoint get read(Comprobación)</h4>
+<img src="image4.png"></img>
--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-from typing import List
-from fastapi import FastAPI
-from services import read
+from fastapi import FastAPI, Depends
+from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
+from services import user
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
 
-@app.get("/root", response_model=List[dict])
-async def root():
-    result = read.registre()
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
+              
+def read_user(db:Session = Depends(get_db)):
+    result = user.get_all_users(db)
+    return result
+
+
+@app.get("/users/")
+def read_user(db: Session = Depends(get_db)):
+    result = user.get_all_users(db)
+    return result
+
+@app.post("/users/")
+def create_user(name: str, email: str, db: Session = Depends(get_db)):
+    result = user.add_new_user(name, email, db)
     return result
