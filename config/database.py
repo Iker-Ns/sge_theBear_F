7a80f91b453bbf64682f995e180fcf700aa1951d
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-04-08 08:54:04 +0200

    database

--- config/database.py
+++ config/database.py
@@ -1,12 +1,27 @@
-import psycopg2
+from os import getenv
+from typing import Generator
+from sqlmodel import Session, create_engine, SQLModel
 
-def connection_db():
-   conn = psycopg2.connect(
-       database="the_bear",
-       password="admin",
-       user="admin",
-       host="localhost",
-       port="5432" 
-    )
-       
-   return conn
+class Database:
+   """
+   Permite conectar a la base de datos.
+
+   Funciones
+   ---------
+   get_session() -> Generator[Session]
+      Devuelve una sesión de conexión a la base de datos.
+   """
+   _uri = getenv("DATABASE_URI")
+   _engine = create_engine(_uri)
+   SQLModel.metadata.create_all(_engine) #Crea las tablas en la base de datos si no existen
+
+   @staticmethod
+   def get_session() -> Generator[Session]:
+      session = Session(Database._engine)
+      try:
+         yield session
+      finally:
+         session.close()
+
+   def __new__(cls):
+      raise TypeError("This class cannot be instantiated")
