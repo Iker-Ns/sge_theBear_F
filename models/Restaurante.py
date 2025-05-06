b81cb34ad24d844db98297e697c00edb51b51ac4
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-05-06 13:22:07 +0200

    Fixes

--- models/Restaurante.py
+++ models/Restaurante.py
@@ -1,5 +1,4 @@
-from sqlmodel import SQLModel, Field, Relationship
-from models.Trabajador import Trabajador
+from sqlmodel import SQLModel, Field
 
 class Restaurante(SQLModel, table=True):
     """"
@@ -19,5 +18,3 @@ class Restaurante(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     direccion: str = Field(max_length=100, nullable=False)
     codigo_postal: int = Field(nullable=False)
-    encargado_id: int = Field(foreign_key="trabajador.id", nullable=False) # Restaurante 1 .. N Trabajadores
-    encargado: Trabajador = Relationship()
